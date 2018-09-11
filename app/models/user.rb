f7bdab608dc0c969c1b72cf8a4b28b23cdbc03b3
lingjie zhang <zhanglingjie16303@gmail.com>
2018-09-11 17:39:47 -0400

    after

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   attr_reader :password
 
-  before_initialize :ensure_session_token
+  after_initialize :ensure_session_token
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
