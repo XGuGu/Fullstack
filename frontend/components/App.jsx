c48956318224cd5a17a3047680adf69e0b67338d
lingjie zhang <zhanglingjie16303@gmail.com>
2018-09-11 16:52:18 -0400

    Working on Auth

---
+++ frontend/components/App.jsx
@@ -0,0 +1,28 @@
+// import React from 'react';
+// import { Provider } from 'react-redux';
+// import {
+//   Route,
+//   Redirect,
+//   Switch,
+//   Link,
+//   HashRouter
+// } from 'react-router-dom';
+//
+// import SessionBackground from './session_form/session_background';
+// import { AuthRoute, ProtectedRoute} from '../util/route_util';
+// import UserProfileContainer from './user_profile/user_profile_container';
+//
+//
+// const App = () => (
+//   <div className='content'>
+//
+//     <Switch>
+//       <AuthRoute path="/(signup|login)/" component={SessionBackground}/>
+//       <ProtectedRoute exact path="/users/:userId" component={UserProfileContainer}/>
+//       <Redirect to="/"></Redirect>
+//     </Switch>
+//
+//   </div>
+// );
+//
+// export default App;
