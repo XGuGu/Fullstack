1db658307f3b53c1a83894039a388a561d947548
lingjie zhang <zhanglingjie16303@gmail.com>
2018-09-19 15:08:05 -0400

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           query: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+            presets: ['@babel/env', '@babel/react']
           }
         },
         exclude: /node_modules/
